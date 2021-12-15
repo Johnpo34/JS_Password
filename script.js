3dceb93dace103cd74af93227d8bf7187cfca33e
John Pohl <johnpohl34@gmail.com>
2021-12-15 10:54:11 -0500

    getting password length

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ function writePassword() {
 }
 
 function generatePassword() {
-
   
+  var pwdLength = prompt("Between 8 and 128 how long would you like your password to be?")
+
+  while (pwdLength < 8 || pwdLength > 128) {
+    alert("Your password must be a number between 8 and 128, try again.")
+    pwdLength = prompt("Between 8 and 128 how long would you like your password to be?")
+  }
 
   console.log(pwdLength)
 }
